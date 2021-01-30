53f8e2d8618f17500116782a3fd631d62d01774e
PlasmaWreck <31967552+PlasmaWreck@users.noreply.github.com>
2021-01-30 13:50:15 -0800

    Testing

--- Assets/Scripts/PushScript.cs
+++ Assets/Scripts/PushScript.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //hi
-public class StartingScript : MonoBehaviour
+public class PushScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject Box;
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Box.transform.Translate(Vector3.forward * Time.deltaTime);
     }
 }
